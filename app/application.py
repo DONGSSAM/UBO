1b09d627f26722a2c8253632ad22ae1562e43617
DONGSSAM <dkdk137882@gmail.com>
2025-11-10 00:52:28 +0900

    개발버전으로 수정 완료

--- app/application.py
+++ app/application.py
@@ -1,14 +1,12 @@
-from gevent import monkey
-monkey.patch_all()
 from flask import Flask, render_template, request, Response, url_for, redirect, session, jsonify, send_file
 from datetime import datetime
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import sys, bcrypt, qrcode, random, os
-from .db import users, check_connection,  chat_rooms, fs
+from db import users, check_connection,  chat_rooms, fs
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 102 # 500MB제한
 app.config['SECRET_KEY'] = "your-very-secret-key"
@@ -934,5 +932,4 @@ def give_mission_points():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=5000)
